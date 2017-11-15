4a169ec65cfa632be424cd6b85005ec3115837f3
Mary6520 <31927004+melania613@users.noreply.github.com>
2017-11-15 15:06:35 +0100

    Kocik zjada pizze - gotowa
    gotowa

--- Kocik zjada pizzę/Kot.java
+++ Kocik zjada pizzę/Kot.java
@@ -1,11 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Kot here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Kot extends Actor
 {
     //sekcja stałych
@@ -18,23 +12,36 @@ public class Kot extends Actor
 
     //sekcja zmiennych
     int stanKot = STAN_SIEDZI;
+    int punkty = 0;
+    boolean tanczyc = false;
+
     public void ruch()
     {
-        if ( Greenfoot.isKeyDown ("right") ) move(2);
+        if ( Greenfoot.isKeyDown ("right") ) 
+
         {
+            move(5);
             if(stanKot == STAN_W_PRAWO_1)
                 stanKot = STAN_W_PRAWO_2;
             else
                 stanKot = STAN_W_PRAWO_1;
         }
+        //koniec "if" w prawo
+        if ( Greenfoot.isKeyDown ("left") ) 
 
-        if ( Greenfoot.isKeyDown ("left") ) move(-2);
         {
+            move(-5);
             if(stanKot == STAN_W_LEWO_1)
                 stanKot = STAN_W_LEWO_2;
             else
                 stanKot = STAN_W_LEWO_1;  
         }
+        //koniec "if" w lewo
+        if ( !Greenfoot.isKeyDown ("left") && !Greenfoot.isKeyDown ("right") ) 
+        {
+            stanKot =STAN_SIEDZI;
+        }
+        //koniec "if" siedzi
     }
     //koniec ruchu
     public void zjadajPizze()
@@ -42,35 +49,58 @@ public class Kot extends Actor
         if (isTouching(Pizza.class) )
         {
             removeTouching(Pizza.class);
+            punkty++;
+            tanczyc = true;
         }
     }
-
+    //koniec zjedz pizze
     public void ustawWyglad()
     {
         if(stanKot == STAN_SIEDZI)
             setImage("cat-sit.png");
-            
+
         if(stanKot == STAN_W_PRAWO_1)
             setImage("cat-walk-right.png");
-            
+
         if(stanKot == STAN_W_PRAWO_2)
             setImage("cat-walk-right2.png");
-            
+
         if(stanKot == STAN_W_LEWO_1)
             setImage("cat-walk-left.png");
-            
+
         if(stanKot == STAN_W_LEWO_2)
             setImage("cat-walk-left2.png");
     }
+    //koniec ustawWyglad
 
-    /**
-     * Act - do whatever the Kot wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    public void pokazPunkty()
+
+    {
+        World world = this.getWorld();
+        world.showText("Punkty:" + punkty, 100, 50);
+    }
+    //koniecpokazPunkty
+    public void taniec()
+
+    {
+        if((punkty % 10 == 0) && tanczyc)
+        {
+            setImage("cat-dance.png");
+            Greenfoot.delay(10);
+            setImage("cat-dance-2.png");
+            Greenfoot.delay(10);
+            tanczyc = false;
+        }
+
+    }
+    //koniec taniec
     public void act() 
     {
+        ustawWyglad();
         ruch();
         zjadajPizze();
-        ustawWglad();
+        pokazPunkty();
+        taniec();
     }    
+    //koniec act
 }
